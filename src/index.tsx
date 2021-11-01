f64b55092ac8fc2477c64b783ee89682c13fab17
Mandi <mandi018@gmail.com>
2021-10-31 20:08:55 -0500

    Shortened bootstrap import path

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
